6db9806b63a030eb8f63c44bff556e3ef33adced
Jack Long <putheara.long@gmail.com>
2020-03-08 15:51:45 -0400

    got input data to be stored in local storage

--- myScript.js
+++ myScript.js
@@ -9,3 +9,28 @@ showDate.innerHTML = todayDate;
 showClock9am.innerHTML = todayDate;
 showClock10am.innerHTML = todayDate;
 
+$(document).ready(function(){
+  
+    var eventArray = [];
+   if (eventArray === null){
+      eventArray = ""}
+        else eventArray = JSON.parse(localStorage.getItem("occasion"));
+    
+    $("#saveButton1").click(function(event){
+        event.preventDefault();
+        var comment = $("#plannedEvent1").val();
+        eventArray.push(comment);
+        localStorage.setItem("occasion", JSON.stringify(eventArray));
+        
+    });
+    
+    $("#saveButton2").click(function(event){
+        event.preventDefault();
+        var comment = $("#plannedEvent2").val();
+        eventArray.push(comment);
+        localStorage.setItem("occasion", JSON.stringify(eventArray));
+        
+    });
+});
+
+console.log(localStorage.getItem("occasion"));
