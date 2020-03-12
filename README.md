34d527f50c032f5cadcf2eb7ea23e502c23b9bc3
Jack Long <putheara.long@gmail.com>
2020-03-11 23:29:07 -0400

    the best i can do

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # homework-week5
+
+the homework is to make a daily planner with timeblock turning one color if it's in the past, another color for the present, and a different color for the future.
+
+i got the clock to show where is supposed to be.
+
+my time blocks are all one color for whatever reason.
+
+i got the local storage to work, but i could call the value so that it would stay on the page even after the page is refreshed.
+
+no time for css.
--- myScript.js
+++ myScript.js
@@ -3,44 +3,52 @@ var showDate = document.getElementById("currentDay");
 var todayDate = moment().format("dddd, MMMM Do YYYY");
 var showClock= $(".clockDisplay");
 var showClock10am = document.getElementById("10am");
-var blockTime = parseInt($(".xyz").attr("value"));
+var blockTime = $(".time-block").data("time");
 
 
-function liveTime() {
-var clock = moment().format("HH:mm:ss");
-showClock.innerHTML = clock;
+//function liveTime() {
+//var clock = moment().format("HH:mm:ss");
+//showClock.innerHTML = clock;
 
-};
-setInterval(liveTime, 1000);
-liveTime();
-showClock10am.innerHTML = todayDate;
+//};
+//setInterval(liveTime, 1000);
+//liveTime();
+//showClock10am.innerHTML = todayDate;
 currentHour = moment().hour();
 //console.log(blockTime);
-//console.log(currentHour);
+
 
 showDate.innerHTML = todayDate;
 
-$.each($(".time-block"), function(i){
-    if (blockTime < currentHour){
-        $(".time-block").css("background-color", "gray");
+var timeElement = $(".time-block");
+for(var i=0; i < timeElement.length; i++){
+    var blockTime = parseInt(timeElement.eq(i).data("time"));
+    if (blockTime < currentHour) {
+        timeElement.css("background-color", "gray");
     }
     else if (blockTime === currentHour) {
-        $(".time-block").css("background-color", "yellow");
+        timeElement.css("background-color", "blue");
     }
-    else $(".time-block").css("background-color", "green");
-    
-});
+    else timeElement.css("background-color", "green");
+
+};
+
+
 
 
 
 
 $(document).ready(function(){
-  
+    var storedArray = JSON.parse(localStorage.getItem("occasion"))
     var eventArray = [];
    if (localStorage.getItem("occasion") === null){
        eventArray = []}
     else eventArray = JSON.parse(localStorage.getItem("occasion"));
      
+    function checkInput(storedArray) {
+        return activity
+    
+    };
     
     $("#saveButton1").click(function(event){
         event.preventDefault();        
@@ -49,8 +57,81 @@ $(document).ready(function(){
             return } 
         else  eventArray.push(activity);
             localStorage.setItem("occasion", JSON.stringify(eventArray));
-        
-        
+       
+        $("#plannedEvent1").val(storedArray.find(checkInput));    });
+
+    console.log($("#plannedEvent1").val(storedArray.find(checkInput())));
+    
+    $("#saveButton2").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent2").val();
+        if ($("#plannedEvent2").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton3").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent3").val();
+        if ($("#plannedEvent3").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton4").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent4").val();
+        if ($("#plannedEvent4").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton5").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent5").val();
+        if ($("#plannedEvent5").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton6").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent6").val();
+        if ($("#plannedEvent6").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton7").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent7").val();
+        if ($("#plannedEvent7").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton8").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent8").val();
+        if ($("#plannedEvent8").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
+    });
+
+    $("#saveButton9").click(function(event){
+        event.preventDefault();        
+        var activity = $("#plannedEvent9").val();
+        if ($("#plannedEvent9").val() === "") {
+            return } 
+        else  eventArray.push(activity);
+            localStorage.setItem("occasion", JSON.stringify(eventArray));  
     });
     //$("#plannedEvent1").prop("disabled", true);
 
